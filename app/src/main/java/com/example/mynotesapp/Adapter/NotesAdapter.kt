c9dc71708eb701e66e76a208e8a9733a505533cc
Vikash.Jerai <vikashjerai2034@gmail.com>
2023-07-18 12:51:22 +0530

    Updated

--- app/src/main/java/com/example/mynotesapp/Adapter/NotesAdapter.kt
+++ app/src/main/java/com/example/mynotesapp/Adapter/NotesAdapter.kt
@@ -9,19 +9,20 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mynotesapp.Models.Note
 import com.example.mynotesapp.R
+import kotlin.random.Random
 
 class NotesAdapter(private val context:Context, val listener: NotesClickListener): RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
 
     private val NotesList=ArrayList<Note>()
     private val fullList=ArrayList<Note>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesAdapter.NoteViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         return NoteViewHolder(
             LayoutInflater.from(context).inflate(R.layout.list_item,parent,false)
         )
     }
 
-    override fun onBindViewHolder(holder: NotesAdapter.NoteViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
        val currentNote= NotesList[position]
         holder.title.text=currentNote.title
         holder.title.isSelected=true
@@ -29,10 +30,11 @@ class NotesAdapter(private val context:Context, val listener: NotesClickListener
         holder.date.text=currentNote.date
         holder.date.isSelected=true
 
+        holder.notes_layout.setCardBackgroundColor(holder.itemView.resources.getColor(randomColor(),null))
         holder.notes_layout.setOnClickListener{
             listener.OnItemClicked(NotesList[holder.adapterPosition])
         }
-        holder.notes_layout.setOnClickListener{
+        holder.notes_layout.setOnLongClickListener{
             listener.OnItemLongClicked(NotesList[holder.adapterPosition],holder.notes_layout)
             true
         }
@@ -40,6 +42,21 @@ class NotesAdapter(private val context:Context, val listener: NotesClickListener
 
     override fun getItemCount(): Int {
         return NotesList.size
+    }
+
+    fun randomColor():Int{
+        val list=ArrayList<Int>()
+        list.add(R.color.note_color_1_blue)
+        list.add(R.color.note_color_2_green)
+        list.add(R.color.note_color_3_violet)
+        list.add(R.color.note_color_4_orange)
+        list.add(R.color.note_color_5_yellow)
+        list.add(R.color.note_color_6_red)
+        list.add(R.color.note_color_7_blueGrey)
+
+        val seed=System.currentTimeMillis().toInt()
+        val randomIndex= Random(seed).nextInt(list.size)
+        return list[randomIndex]
     }
 
     fun updateList(newList: List<Note>){
